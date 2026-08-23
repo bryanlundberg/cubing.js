dfb3a9977a82245f66be76cef4bbdb9c3f4a3cd9
Lucas Garron <code@garron.net>
2026-08-23 20:34:19 +0200

    [puzzles] Change the 4×4×4 LL SVG to use exact coordinates instead of serialized floating point approximations.

--- src/cubing/puzzles/implementations/dynamic/4x4x4/4x4x4-ll.kpuzzle.svg.ts
+++ src/cubing/puzzles/implementations/dynamic/4x4x4/4x4x4-ll.kpuzzle.svg.ts
@@ -8,9 +8,9 @@ export const cube4x4x4LLSVG = `<svg
   <g fill="none" fill-rule="nonzero" stroke="#000">
     <g stroke-linejoin="round" stroke-width="3" transform="translate(17 17)">
       <!-- U -->
-      <path id="CORNERS-l1-o0" d="m184.6 147.8v36.8h-36.8v-36.8z" style="fill: white" />
-      <path id="EDGES-l10-o0" d="m147.8 184.6h-36.8v-36.8h36.8z" style="fill: white" />
-      <path id="EDGES-l0-o0" d="m111 147.8v36.8h-36.8v-36.8z" style="fill: white" />
+      <path id="CORNERS-l2-o0" d="m184.6 147.8v36.8h-36.8v-36.8z" style="fill: white" />
+      <path id="EDGES-l0-o0" d="m147.8 184.6h-36.8v-36.8h36.8z" style="fill: white" />
+      <path id="EDGES-l16-o0" d="m111 147.8v36.8h-36.8v-36.8z" style="fill: white" />
       <path id="CORNERS-l0-o0" d="m74.2 184.6h-36.8v-36.8h36.8z" style="fill: white" />
       <path id="EDGES-l6-o0" d="m37.4 147.8v-36.8h36.8v36.8z" style="fill: white" />
       <path id="CENTERS-l10-o0" d="m74.2 111h36.8v36.8h-36.8z" style="fill: white" />
@@ -25,25 +25,25 @@ export const cube4x4x4LLSVG = `<svg
       <path id="EDGES-l18-o0" d="m111 74.2v-36.8h36.8v36.8z" style="fill: white" />
       <path id="CORNERS-l3-o0" d="m147.8 37.4h36.8v36.8h-36.8z" style="fill: white" />
 
-      <path id="CORNERS-l0-o1" d="m.6000061 221.399997 36.7999939-36.8h36.8v36.8z" style="fill: limegreen" />
-      <path id="EDGES-l0-o1" d="m74.2 184.599997h36.8v36.8h-36.8z" style="fill: limegreen" />
-      <path id="EDGES-l10-o1" d="m111 221.399997v-36.8h36.8v36.8z" style="fill: limegreen" />
-      <path id="CORNERS-l1-o2" d="m147.8 184.599997h36.8l36.799994 36.8h-73.599994z" style="fill: limegreen" />
+      <path id="CORNERS-l0-o1" d="m.6 221.4 36.8-36.8h36.8v36.8z" style="fill: limegreen" />
+      <path id="EDGES-l0-o1" d="m74.2 184.5h36.8v36.8h-36.8z" style="fill: limegreen" />
+      <path id="EDGES-l10-o1" d="m111 221.4v-36.8h36.8v36.8z" style="fill: limegreen" />
+      <path id="CORNERS-l1-o2" d="m147.8 184.5h36.8l36.8 36.8h-73.6z" style="fill: limegreen" />
 
-      <path id="CORNERS-l0-o2" d="m-17.7999969 166.2h36.8l36.7999938 36.8h-73.5999938z" style="fill: orange" transform="matrix(0 1 -1 0 203.600003 165.599997)" />
-      <path id="EDGES-l6-o1" d="m.60000305 147.8v-36.8h36.80000005v36.8z" style="fill: orange" transform="matrix(0 1 -1 0 148.400003 110.399997)" />
-      <path id="EDGES-l22-o1" d="m.60000305 74.2h36.80000005v36.8h-36.80000005z" style="fill: orange" transform="matrix(0 1 -1 0 111.600003 73.599997)" />
-      <path id="CORNERS-l6-o1" d="m-17.7999966 55.8 36.7999997-36.8h36.8v36.8z" style="fill: orange" transform="matrix(0 1 -1 0 56.400003 18.399997)" />
+      <path id="CORNERS-l0-o2" d="m-18 166.2h36.8l36.8 36.8h-73.6z" style="fill: orange" transform="matrix(0 1 -1 0 203.6 165.5)" />
+      <path id="EDGES-l6-o1" d="m.6 147.8v-36.8h36.8v36.8z" style="fill: orange" transform="matrix(0 1 -1 0 148.4 110.4)" />
+      <path id="EDGES-l22-o1" d="m.6 74.2h36.8v36.8h-36.8z" style="fill: orange" transform="matrix(0 1 -1 0 111.6 73.5)" />
+      <path id="CORNERS-l6-o1" d="m-18 55.8 36.8-36.8h36.8v36.8z" style="fill: orange" transform="matrix(0 1 -1 0 56.4 18.4)" />
 
-      <path id="CORNERS-l3-o1" d="m147.800006 37.3999992 36.799994-36.79999996h36.8v36.79999996z" style="fill: #26f" transform="matrix(-1 0 0 -1 369.2 37.999998)" />
-      <path id="EDGES-l18-o1" d="m111 .59999924h36.8v36.79999996h-36.8z" style="fill: #26f" transform="matrix(-1 0 0 -1 258.8 37.999998)" />
-      <path id="EDGES-l15-o1" d="m74.2 37.3999992v-36.79999996h36.8v36.79999996z" style="fill: #26f" transform="matrix(-1 0 0 -1 185.2 37.999998)" />
-      <path id="CORNERS-l6-o2" d="m.6.59999924h36.8l36.7999939 36.79999996h-73.5999939z" style="fill: #26f" transform="matrix(-1 0 0 -1 74.8 37.999998)" />
+      <path id="CORNERS-l3-o1" d="m147.8 37.4 36.8-36.8h36.8v36.8z" style="fill: #26f" transform="matrix(-1 0 0 -1 369.2 38)" />
+      <path id="EDGES-l18-o1" d="m111 .6h36.8v36.8h-36.8z" style="fill: #26f" transform="matrix(-1 0 0 -1 258.8 38)" />
+      <path id="EDGES-l15-o1" d="m74.2 37.4v-36.8h36.8v36.8z" style="fill: #26f" transform="matrix(-1 0 0 -1 185.2 38)" />
+      <path id="CORNERS-l6-o2" d="m.6.6h36.8l36.8 36.8h-73.6z" style="fill: #26f" transform="matrix(-1 0 0 -1 74.8 38)" />
 
-      <path id="CORNERS-l1-o1" d="m166.200018 203 36.799994-36.8h36.8v36.8z" style="fill: red" transform="matrix(0 -1 1 0 18.400012 387.600012)" />
-      <path id="EDGES-l2-o1" d="m184.600012 111h36.8v36.8h-36.8z" style="fill: red" transform="matrix(0 -1 1 0 73.600012 332.400012)" />
-      <path id="EDGES-l21-o1" d="m184.600012 111v-36.8h36.8v36.8z" style="fill: red" transform="matrix(0 -1 1 0 110.400012 295.600012)" />
-      <path id="CORNERS-l3-o2" d="m166.200012 19h36.8l36.8 36.8h-73.6z" style="fill: red" transform="matrix(0 -1 1 0 165.600012 240.400012)" />
+      <path id="CORNERS-l1-o1" d="m166.200018 203 36.8-36.8h36.8v36.8z" style="fill: red" transform="matrix(0 -1 1 0 18.4 387.6)" />
+      <path id="EDGES-l2-o1" d="m184.6 111h36.8v36.8h-36.8z" style="fill: red" transform="matrix(0 -1 1 0 73.6 332.4)" />
+      <path id="EDGES-l21-o1" d="m184.6 111v-36.8h36.8v36.8z" style="fill: red" transform="matrix(0 -1 1 0 110.4 295.6)" />
+      <path id="CORNERS-l3-o2" d="m166.200012 19h36.8l36.8 36.8h-73.6z" style="fill: red" transform="matrix(0 -1 1 0 165.6 240.4)" />
     </g>
     <g opacity="0" transform="translate(17 242)">
       <path id="EDGES-l1-o0" d="m12 0v1h-1v-1z" style="fill: limegreen" />
