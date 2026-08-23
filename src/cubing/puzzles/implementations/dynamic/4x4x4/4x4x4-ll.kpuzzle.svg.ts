b6f292b12c65f272229fca3c0c73745f8a0063e7
Lucas Garron <code@garron.net>
2026-08-23 20:47:11 +0200

    [puzzles] Update the 4×4×4 LL SVG to be compatible with Speffz.
    Fixes https://github.com/cubing/cubing.js/issues/417

--- src/cubing/puzzles/implementations/dynamic/4x4x4/4x4x4-ll.kpuzzle.svg.ts
+++ src/cubing/puzzles/implementations/dynamic/4x4x4/4x4x4-ll.kpuzzle.svg.ts
@@ -5,115 +5,123 @@ export const cube4x4x4LLSVG = `<svg
   xmlns="http://www.w3.org/2000/svg"
   xmlns:xlink="http://www.w3.org/1999/xlink"
 >
+  <defs>
+    <g id="sticker">
+        <rect x="0" y="0" width="1" height="1"/>
+    </g>
+  </defs>
   <g fill="none" fill-rule="nonzero" stroke="#000">
     <g stroke-linejoin="round" stroke-width="3" transform="translate(17 17)">
       <!-- U -->
-      <path id="CORNERS-l2-o0" d="m184.6 147.8v36.8h-36.8v-36.8z" style="fill: white" />
-      <path id="EDGES-l0-o0" d="m147.8 184.6h-36.8v-36.8h36.8z" style="fill: white" />
-      <path id="EDGES-l16-o0" d="m111 147.8v36.8h-36.8v-36.8z" style="fill: white" />
-      <path id="CORNERS-l0-o0" d="m74.2 184.6h-36.8v-36.8h36.8z" style="fill: white" />
-      <path id="EDGES-l6-o0" d="m37.4 147.8v-36.8h36.8v36.8z" style="fill: white" />
-      <path id="CENTERS-l10-o0" d="m74.2 111h36.8v36.8h-36.8z" style="fill: white" />
-      <path id="CENTERS-l21-o0" d="m111 147.8v-36.8h36.8v36.8z" style="fill: white" />
-      <path id="EDGES-l2-o0" d="m147.8 111h36.8v36.8h-36.8z" style="fill: white" />
-      <path id="EDGES-l21-o0" d="m184.6 74.2v36.8h-36.8v-36.8z" style="fill: white" />
-      <path id="CENTERS-l15-o0" d="m147.8 111h-36.8v-36.8h36.8z" style="fill: white" />
-      <path id="CENTERS-l6-o0" d="m111 74.2v36.8h-36.8v-36.8z" style="fill: white" />
-      <path id="EDGES-l22-o0" d="m74.2 111h-36.8v-36.8h36.8z" style="fill: white" />
-      <path id="CORNERS-l6-o0" d="m37.4 74.2v-36.8h36.8v36.8z" style="fill: white" />
-      <path id="EDGES-l15-o0" d="m74.2 37.4h36.8v36.8h-36.8z" style="fill: white" />
-      <path id="EDGES-l18-o0" d="m111 74.2v-36.8h36.8v36.8z" style="fill: white" />
-      <path id="CORNERS-l3-o0" d="m147.8 37.4h36.8v36.8h-36.8z" style="fill: white" />
+      <path id="CORNERS-l2-o0" d="m37.4 74.2v-36.8h36.8v36.8z" style="fill: white" />
+      <path id="EDGES-l0-o0" d="m74.2 37.4h36.8v36.8h-36.8z" style="fill: white" />
+      <path id="EDGES-l16-o0" d="m111 74.2v-36.8h36.8v36.8z" style="fill: white" />
+      <path id="CORNERS-l1-o0" d="m147.8 37.4h36.8v36.8h-36.8z" style="fill: white" />
 
-      <path id="CORNERS-l0-o1" d="m.6 221.4 36.8-36.8h36.8v36.8z" style="fill: limegreen" />
-      <path id="EDGES-l0-o1" d="m74.2 184.5h36.8v36.8h-36.8z" style="fill: limegreen" />
-      <path id="EDGES-l10-o1" d="m111 221.4v-36.8h36.8v36.8z" style="fill: limegreen" />
-      <path id="CORNERS-l1-o2" d="m147.8 184.5h36.8l36.8 36.8h-73.6z" style="fill: limegreen" />
+      <path id="EDGES-l4-o0" d="m74.2 111h-36.8v-36.8h36.8z" style="fill: white" />
+      <path id="CENTERS-l0-o0" d="m111 74.2v36.8h-36.8v-36.8z" style="fill: white" />
+      <path id="CENTERS-l1-o0" d="m147.8 111h-36.8v-36.8h36.8z" style="fill: white" />
+      <path id="EDGES-l1-o0" d="m184.6 74.2v36.8h-36.8v-36.8z" style="fill: white" />
 
-      <path id="CORNERS-l0-o2" d="m-18 166.2h36.8l36.8 36.8h-73.6z" style="fill: orange" transform="matrix(0 1 -1 0 203.6 165.5)" />
-      <path id="EDGES-l6-o1" d="m.6 147.8v-36.8h36.8v36.8z" style="fill: orange" transform="matrix(0 1 -1 0 148.4 110.4)" />
-      <path id="EDGES-l22-o1" d="m.6 74.2h36.8v36.8h-36.8z" style="fill: orange" transform="matrix(0 1 -1 0 111.6 73.5)" />
-      <path id="CORNERS-l6-o1" d="m-18 55.8 36.8-36.8h36.8v36.8z" style="fill: orange" transform="matrix(0 1 -1 0 56.4 18.4)" />
+      <path id="EDGES-l3-o0" d="m37.4 147.8v-36.8h36.8v36.8z" style="fill: white" />
+      <path id="CENTERS-l3-o0" d="m74.2 111h36.8v36.8h-36.8z" style="fill: white" />
+      <path id="CENTERS-l2-o0" d="m111 147.8v-36.8h36.8v36.8z" style="fill: white" />
+      <path id="EDGES-l12-o0" d="m147.8 111h36.8v36.8h-36.8z" style="fill: white" />
 
-      <path id="CORNERS-l3-o1" d="m147.8 37.4 36.8-36.8h36.8v36.8z" style="fill: #26f" transform="matrix(-1 0 0 -1 369.2 38)" />
-      <path id="EDGES-l18-o1" d="m111 .6h36.8v36.8h-36.8z" style="fill: #26f" transform="matrix(-1 0 0 -1 258.8 38)" />
-      <path id="EDGES-l15-o1" d="m74.2 37.4v-36.8h36.8v36.8z" style="fill: #26f" transform="matrix(-1 0 0 -1 185.2 38)" />
-      <path id="CORNERS-l6-o2" d="m.6.6h36.8l36.8 36.8h-73.6z" style="fill: #26f" transform="matrix(-1 0 0 -1 74.8 38)" />
+      <path id="CORNERS-l3-o0" d="m74.2 184.6h-36.8v-36.8h36.8z" style="fill: white" />
+      <path id="EDGES-l8-o0" d="m111 147.8v36.8h-36.8v-36.8z" style="fill: white" />
+      <path id="EDGES-l2-o0" d="m147.8 184.6h-36.8v-36.8h36.8z" style="fill: white" />
+      <path id="CORNERS-l0-o0" d="m184.6 147.8v36.8h-36.8v-36.8z" style="fill: white" />
 
-      <path id="CORNERS-l1-o1" d="m166.200018 203 36.8-36.8h36.8v36.8z" style="fill: red" transform="matrix(0 -1 1 0 18.4 387.6)" />
-      <path id="EDGES-l2-o1" d="m184.6 111h36.8v36.8h-36.8z" style="fill: red" transform="matrix(0 -1 1 0 73.6 332.4)" />
-      <path id="EDGES-l21-o1" d="m184.6 111v-36.8h36.8v36.8z" style="fill: red" transform="matrix(0 -1 1 0 110.4 295.6)" />
-      <path id="CORNERS-l3-o2" d="m166.200012 19h36.8l36.8 36.8h-73.6z" style="fill: red" transform="matrix(0 -1 1 0 165.6 240.4)" />
+      <path id="CORNERS-l3-o1" d="m.6 221.4 36.8-36.8h36.8v36.8z" style="fill: limegreen" />
+      <path id="EDGES-l8-o1" d="m74.2 184.5h36.8v36.8h-36.8z" style="fill: limegreen" />
+      <path id="EDGES-l2-o1" d="m111 221.4v-36.8h36.8v36.8z" style="fill: limegreen" />
+      <path id="CORNERS-l0-o2" d="m147.8 184.5h36.8l36.8 36.8h-73.6z" style="fill: limegreen" />
+
+      <path id="CORNERS-l2-o1" d="m-18 55.8 36.8-36.8h36.8v36.8z" style="fill: orange" transform="matrix(0 1 -1 0 56.4 18.4)" />
+      <path id="EDGES-l4-o1" d="m.6 74.2h36.8v36.8h-36.8z" style="fill: orange" transform="matrix(0 1 -1 0 111.6 73.5)" />
+      <path id="EDGES-l3-o1" d="m.6 147.8v-36.8h36.8v36.8z" style="fill: orange" transform="matrix(0 1 -1 0 148.4 110.4)" />
+      <path id="CORNERS-l3-o2" d="m-18 166.2h36.8l36.8 36.8h-73.6z" style="fill: orange" transform="matrix(0 1 -1 0 203.6 165.5)" />
+
+      <path id="CORNERS-l1-o1" d="m147.8 37.4 36.8-36.8h36.8v36.8z" style="fill: #26f" transform="matrix(-1 0 0 -1 369.2 38)" />
+      <path id="EDGES-l16-o1" d="m111 .6h36.8v36.8h-36.8z" style="fill: #26f" transform="matrix(-1 0 0 -1 258.8 38)" />
+      <path id="EDGES-l0-o1" d="m74.2 37.4v-36.8h36.8v36.8z" style="fill: #26f" transform="matrix(-1 0 0 -1 185.2 38)" />
+      <path id="CORNERS-l2-o2" d="m.6.6h36.8l36.8 36.8h-73.6z" style="fill: #26f" transform="matrix(-1 0 0 -1 74.8 38)" />
+
+      <path id="CORNERS-l0-o1" d="m166.200018 203 36.8-36.8h36.8v36.8z" style="fill: red" transform="matrix(0 -1 1 0 18.4 387.6)" />
+      <path id="EDGES-l12-o1" d="m184.6 111h36.8v36.8h-36.8z" style="fill: red" transform="matrix(0 -1 1 0 73.6 332.4)" />
+      <path id="EDGES-l1-o1" d="m184.6 111v-36.8h36.8v36.8z" style="fill: red" transform="matrix(0 -1 1 0 110.4 295.6)" />
+      <path id="CORNERS-l1-o2" d="m166.200012 19h36.8l36.8 36.8h-73.6z" style="fill: red" transform="matrix(0 -1 1 0 165.6 240.4)" />
     </g>
     <g opacity="0" transform="translate(17 242)">
-      <path id="EDGES-l1-o0" d="m12 0v1h-1v-1z" style="fill: limegreen" />
-      <path id="CENTERS-l0-o0" d="m10 1h-1v-1h1z" style="fill: limegreen" />
-      <path id="CENTERS-l23-o0" d="m9 0v1h-1v-1z" style="fill: limegreen" />
-      <path id="EDGES-l20-o0" d="m7 1h-1v-1h1z" style="fill: limegreen" />
-      <path id="EDGES-l23-o0" d="m6 2v-1h1v1z" style="fill: limegreen" />
-      <path id="CENTERS-l14-o0" d="m8 1h1v1h-1z" style="fill: limegreen" />
-      <path id="CENTERS-l1-o0" d="m9 2v-1h1v1z" style="fill: limegreen" />
-      <path id="EDGES-l7-o0" d="m11 1h1v1h-1z" style="fill: limegreen" />
-      <path id="CORNERS-l4-o1" d="m12 3v1h-1v-1z" style="fill: limegreen" />
-      <path id="EDGES-l14-o1" d="m10 4h-1v-1h1z" style="fill: limegreen" />
-      <path id="EDGES-l3-o1" d="m9 3v1h-1v-1z" style="fill: limegreen" />
-      <path id="CORNERS-l2-o2" d="m7 4h-1v-1h1z" style="fill: limegreen" />
+      <path id="EDGES-l9-o0" href="#sticker" style="fill: limegreen" />
+      <path id="CENTERS-l9-o0" href="#sticker" style="fill: limegreen" />
+      <path id="CENTERS-l8-o0" href="#sticker" style="fill: limegreen" />
+      <path id="EDGES-l5-o0" href="#sticker" style="fill: limegreen" />
+      <path id="EDGES-l11-o0" href="#sticker" style="fill: limegreen" />
+      <path id="CENTERS-l11-o0" href="#sticker" style="fill: limegreen" />
+      <path id="CENTERS-l10-o0" href="#sticker" style="fill: limegreen" />
+      <path id="EDGES-l15-o0" href="#sticker" style="fill: limegreen" />
+      <path id="CORNERS-l4-o1" href="#sticker" style="fill: limegreen" />
+      <path id="EDGES-l10-o1" href="#sticker" style="fill: limegreen" />
+      <path id="EDGES-l20-o1" href="#sticker" style="fill: limegreen" />
+      <path id="CORNERS-l5-o2" href="#sticker" style="fill: limegreen" />
 
-      <path id="CORNERS-l2-o0" d="m6 6v-1h1v1z" style="fill: yellow"/>
-      <path id="EDGES-l3-o0" d="m8 5h1v1h-1z" style="fill: yellow"/>
-      <path id="EDGES-l14-o0" d="m9 6v-1h1v1z" style="fill: yellow"/>
-      <path id="CORNERS-l4-o0" d="m11 5h1v1h-1z" style="fill: yellow"/>
-      <path id="EDGES-l5-o0" d="m12 7v1h-1v-1z" style="fill: yellow"/>
-      <path id="CENTERS-l3-o0" d="m10 8h-1v-1h1z" style="fill: yellow"/>
-      <path id="CENTERS-l16-o0" d="m9 7v1h-1v-1z" style="fill: yellow"/>
-      <path id="EDGES-l9-o0" d="m7 8h-1v-1h1z" style="fill: yellow"/>
-      <path id="EDGES-l16-o0" d="m6 9v-1h1v1z" style="fill: yellow"/>
-      <path id="CENTERS-l8-o0" d="m8 8h1v1h-1z" style="fill: yellow"/>
-      <path id="CENTERS-l5-o0" d="m9 9v-1h1v1z" style="fill: yellow"/>
-      <path id="EDGES-l12-o0" d="m11 8h1v1h-1z" style="fill: yellow"/>
-      <path id="CORNERS-l5-o0" d="m12 10v1h-1v-1z" style="fill: yellow"/>
-      <path id="EDGES-l8-o0" d="m10 11h-1v-1h1z" style="fill: yellow"/>
-      <path id="EDGES-l19-o0" d="m9 10v1h-1v-1z" style="fill: yellow"/>
-      <path id="CORNERS-l7-o0" d="m7 11h-1v-1h1z" style="fill: yellow"/>
+      <path id="CORNERS-l5-o0" href="#sticker" style="fill: yellow"/>
+      <path id="EDGES-l20-o0" href="#sticker" style="fill: yellow"/>
+      <path id="EDGES-l10-o0" href="#sticker" style="fill: yellow"/>
+      <path id="CORNERS-l4-o0" href="#sticker" style="fill: yellow"/>
+      <path id="EDGES-l21-o0" href="#sticker" style="fill: yellow"/>
+      <path id="CENTERS-l21-o0" href="#sticker" style="fill: yellow"/>
+      <path id="CENTERS-l20-o0" href="#sticker" style="fill: yellow"/>
+      <path id="EDGES-l6-o0" href="#sticker" style="fill: yellow"/>
+      <path id="EDGES-l23-o0" href="#sticker" style="fill: yellow"/>
+      <path id="CENTERS-l23-o0" href="#sticker" style="fill: yellow"/>
+      <path id="CENTERS-l22-o0" href="#sticker" style="fill: yellow"/>
+      <path id="EDGES-l14-o0" href="#sticker" style="fill: yellow"/>
+      <path id="CORNERS-l7-o0" href="#sticker" style="fill: yellow"/>
+      <path id="EDGES-l22-o0" href="#sticker" style="fill: yellow"/>
+      <path id="EDGES-l18-o0" href="#sticker" style="fill: yellow"/>
+      <path id="CORNERS-l6-o0" href="#sticker" style="fill: yellow"/>
 
-      <path id="EDGES-l9-o1" d="m5 0v1h-1v-1z" style="fill: orange" />
-      <path id="CENTERS-l20-o0" d="m4 1h1v1h-1z" style="fill: orange" />
-      <path id="CENTERS-l22-o0" d="m5 3v1h-1v-1z" style="fill: orange" />
-      <path id="EDGES-l20-o1" d="m4 4h-1v-1h1z" style="fill: orange" />
-      <path id="EDGES-l23-o1" d="m3 2v-1h1v1z" style="fill: orange" />
-      <path id="CENTERS-l17-o0" d="m4 1h-1v-1h1z" style="fill: orange" />
-      <path id="CENTERS-l9-o0" d="m2 0v1h-1v-1z" style="fill: orange" />
-      <path id="EDGES-l16-o1" d="m1 1h1v1h-1z" style="fill: orange" />
-      <path id="CORNERS-l7-o2" d="m2 3v1h-1v-1z" style="fill: orange" />
-      <path id="EDGES-l17-o1" d="m1 4h-1v-1h1z" style="fill: orange" />
-      <path id="EDGES-l13-o1" d="m0 2v-1h1v1z" style="fill: orange" />
-      <path id="CORNERS-l2-o1" d="m1 1h-1v-1h1z" style="fill: orange" />
+      <path id="EDGES-l5-o1" href="#sticker" style="fill: orange" />
+      <path id="EDGES-l11-o1" href="#sticker" style="fill: orange" />
+      <path id="CORNERS-l5-o1" href="#sticker" style="fill: orange" />
+      <path id="EDGES-l6-o1" href="#sticker" style="fill: orange" />
+      <path id="CENTERS-l6-o0" href="#sticker" style="fill: orange" />
+      <path id="CENTERS-l5-o0" href="#sticker" style="fill: orange" />
+      <path id="CENTERS-l4-o0" href="#sticker" style="fill: orange" />
+      <path id="CENTERS-l7-o0" href="#sticker" style="fill: orange" />
+      <path id="EDGES-l23-o1" href="#sticker" style="fill: orange" />
+      <path id="CORNERS-l6-o2" href="#sticker" style="fill: orange" />
+      <path id="EDGES-l7-o1" href="#sticker" style="fill: orange" />
+      <path id="EDGES-l17-o1" href="#sticker" style="fill: orange" />
 
-      <path id="EDGES-l11-o0" d="m25 3v1h-1v-1z" style="fill: #26f" />
-      <path id="CENTERS-l19-o0" d="m23 4h-1v-1h1z" style="fill: #26f" />
-      <path id="CENTERS-l13-o0" d="m22 3v1h-1v-1z" style="fill: #26f" />
-      <path id="EDGES-l17-o0" d="m20 4h-1v-1h1z" style="fill: #26f" />
-      <path id="EDGES-l13-o0" d="m19 2v-1h1v1z" style="fill: #26f" />
-      <path id="CENTERS-l18-o0" d="m21 1h1v1h-1z" style="fill: #26f" />
-      <path id="CENTERS-l11-o0" d="m22 2v-1h1v1z" style="fill: #26f" />
-      <path id="EDGES-l4-o0" d="m24 1h1v1h-1z" style="fill: #26f" />
-      <path id="CORNERS-l7-o1" d="m25 0v1h-1v-1z" style="fill: #26f" />
-      <path id="EDGES-l19-o1" d="m23 1h-1v-1h1z" style="fill: #26f" />
-      <path id="EDGES-l8-o1" d="m22 0v1h-1v-1z" style="fill: #26f" />
-      <path id="CORNERS-l5-o2" d="m20 1h-1v-1h1z" style="fill: #26f" />
+      <path id="CORNERS-l6-o1" href="#sticker" style="fill: #26f" />
+      <path id="EDGES-l18-o1" href="#sticker" style="fill: #26f" />
+      <path id="EDGES-l22-o1" href="#sticker" style="fill: #26f" />
+      <path id="CORNERS-l7-o2" href="#sticker" style="fill: #26f" />
+      <path id="EDGES-l19-o0" href="#sticker" style="fill: #26f" />
+      <path id="CENTERS-l19-o0" href="#sticker" style="fill: #26f" />
+      <path id="CENTERS-l18-o0" href="#sticker" style="fill: #26f" />
+      <path id="EDGES-l7-o0" href="#sticker" style="fill: #26f" />
+      <path id="EDGES-l17-o0" href="#sticker" style="fill: #26f" />
+      <path id="CENTERS-l17-o0" href="#sticker" style="fill: #26f" />
+      <path id="CENTERS-l16-o0" href="#sticker" style="fill: #26f" />
+      <path id="EDGES-l13-o0" href="#sticker" style="fill: #26f" />
 
-      <path id="EDGES-l7-o1" d="m14 4h-1v-1h1z" style="fill: red" />
-      <path id="CENTERS-l7-o0" d="m13 2v-1h1v1z" style="fill: red" />
-      <path id="CENTERS-l12-o0" d="m14 1h-1v-1h1z" style="fill: red" />
-      <path id="EDGES-l5-o1" d="m15 0v1h-1v-1z" style="fill: red" />
-      <path id="EDGES-l12-o1" d="m14 1h1v1h-1z" style="fill: red" />
-      <path id="CENTERS-l4-o0" d="m15 3v1h-1v-1z" style="fill: red" />
-      <path id="CENTERS-l2-o0" d="m17 4h-1v-1h1z" style="fill: red" />
-      <path id="EDGES-l1-o1" d="m16 2v-1h1v1z" style="fill: red" />
-      <path id="CORNERS-l4-o2" d="m17 1h-1v-1h1z" style="fill: red" />
-      <path id="EDGES-l4-o1" d="m18 0v1h-1v-1z" style="fill: red" />
-      <path id="EDGES-l11-o1" d="m17 1h1v1h-1z" style="fill: red" />
-      <path id="CORNERS-l5-o1" d="m18 3v1h-1v-1z" style="fill: red" />
+      <path id="CORNERS-l4-o2" href="#sticker" style="fill: red" />
+      <path id="EDGES-l15-o1" href="#sticker" style="fill: red" />
+      <path id="EDGES-l9-o1" href="#sticker" style="fill: red" />
+      <path id="CENTERS-l12-o0" href="#sticker" style="fill: red" />
+      <path id="CENTERS-l15-o0" href="#sticker" style="fill: red" />
+      <path id="EDGES-l21-o1" href="#sticker" style="fill: red" />
+      <path id="EDGES-l14-o1" href="#sticker" style="fill: red" />
+      <path id="CENTERS-l14-o0" href="#sticker" style="fill: red" />
+      <path id="CENTERS-l13-o0" href="#sticker" style="fill: red" />
+      <path id="EDGES-l13-o1" href="#sticker" style="fill: red" />
+      <path id="EDGES-l19-o1" href="#sticker" style="fill: red" />
+      <path id="CORNERS-l7-o1" href="#sticker" style="fill: red" />
     </g>
   </g>
 </svg>`;
