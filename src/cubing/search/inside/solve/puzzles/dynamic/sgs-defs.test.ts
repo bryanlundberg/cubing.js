a0b4b261a50457c2128d05392dc4e33c5b542269
Bryan Lundberg <bryanlundberg@outlook.com>
2026-08-26 20:45:00 -0600

    Update SGS imports in `sgs-defs.test.ts` to use puzzle-specific directories for 2x2x2, pyraminx, and skewb

--- src/cubing/search/inside/solve/puzzles/dynamic/sgs-defs.test.ts
+++ src/cubing/search/inside/solve/puzzles/dynamic/sgs-defs.test.ts
@@ -1,10 +1,8 @@
 import { expect, test } from "bun:test";
 import { SKIP_SLOW_TESTS } from "../../../../../../test/SKIP_SLOW_TESTS";
-import {
-  cachedData222,
-  sgsDataPyraminx,
-  sgsDataSkewb,
-} from "./sgs-side-events/search-dynamic-sgs-side-events";
+import { cachedData222 } from "./sgs-2x2x2/search-dynamic-sgs-2x2x2";
+import { sgsDataPyraminx } from "./sgs-pyraminx/search-dynamic-sgs-pyraminx";
+import { sgsDataSkewb } from "./sgs-skewb/search-dynamic-sgs-skewb";
 
 test.skipIf(SKIP_SLOW_TESTS)("Parses 2x2x2 SGS", () => {
   expect(cachedData222).not.toThrow();
